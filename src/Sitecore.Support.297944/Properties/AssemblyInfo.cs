e33a997908dcf531caa18ed27706f6f1b3e5581f
DK\IEKA <ieka@sitecore.net>
2018-12-06 18:26:55 +0200

    Fix encoding issue for "grid->Component Layout" field.

---
+++ src/Sitecore.Support.297944/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.297944")]
+[assembly: AssemblyProduct("Sitecore.Support.297944")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.297944/XA/Foundation/Grid/Repositories/GridSettingsRepository.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.XA.Foundation.Grid.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.XA.Foundation.Grid.Repositories
+{
+  public class GridSettingsRepository: Sitecore.XA.Foundation.Grid.Repositories.GridSettingsRepository
+  {
+    public override IEnumerable<GridOption> CreateGridModel(Item definitionItem, string values)
+    {
+      #region FIX 297944
+      string valuesDecoded = values;
+      while (valuesDecoded.Contains("%25"))
+      {
+        valuesDecoded = HttpUtility.UrlDecode(valuesDecoded);
+      }
+      if (valuesDecoded.Contains("%7B"))
+      {
+        valuesDecoded = HttpUtility.UrlDecode(valuesDecoded);
+      }
+      #endregion
+
+      List<GridOption> list = CreateGridModel(definitionItem).ToList();
+      if (string.IsNullOrEmpty(values))
+      {
+        return list;
+      }
+      foreach (GridOption item in list)
+      {
+        PopulateValues(item, valuesDecoded);
+      }
+      return list;
+    }
+  }
+}
